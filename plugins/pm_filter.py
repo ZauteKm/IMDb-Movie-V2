68b70ea1f360943f4707b64143b03b7b7212dd1c
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-03-02 12:31:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -30,13 +30,15 @@ logger.setLevel(logging.ERROR)
 BUTTONS = {}
 SPELL_CHECK = {}
 
+@Client.on_message(filters.private & filters.text & ~filters.edited & filters.incoming)
+async def private_filter(client, message):
+        await auto_filter(client, message)
 
 @Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
 async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
         await auto_filter(client, message)
-
 
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
